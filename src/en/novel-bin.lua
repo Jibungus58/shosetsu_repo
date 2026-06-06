ed220898facad8c663d7a4d9c3ab57da5f17566b
Krielies <79584883+Krielies@users.noreply.github.com>
2026-06-06 22:20:53 +0200

    f

--- src/en/novel-bin.lua
+++ src/en/novel-bin.lua
@@ -1,4 +1,4 @@
--- {"id":11151412,"ver":"1.0.2","libVer":"1.0.0","author":"me","repo":"novel-bin"}
+-- {"id":11151412,"ver":"1.1.0","libVer":"1.0.0","author":"me","repo":"novel-bin"}
 
 local baseURL = "https://novel-bin.net/"
 
